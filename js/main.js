d643560bb4dbac1e4f76c7ba8630fde09b69917b
abdo754 <W0786364@myscc.ca>
2023-09-24 01:27:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Function to load content using AJAX and animate
-    function loadContentAndAnimate(url) {
-      var contentDiv = document.getElementById("content");
-      contentDiv.style.opacity = 0; // Hide the content
-  
-      // Use vanilla JavaScript to make an AJAX request
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", url, true);
-  
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          // On success, update the content
-          contentDiv.innerHTML = xhr.responseText;
-  
-          // Animate the content to fade in
-          $(contentDiv).animate({ opacity: 1 }, 500);
-        }
-      };
-  
-      xhr.send();
-    }
-  
-    // Click event for Prospect link
-    document.getElementById("prospect").addEventListener("click", function (e) {
-      e.preventDefault();
-      loadContentAndAnimate("prospect.html");
+$(document).ready(function () {
+  // Function to load content using AJAX and animate the display
+  function loadAndAnimateContent(url) {
+    $('#content').fadeOut('fast', function () { // Animate the fade-out effect
+      $(this).load(url, function () { // Load content using AJAX
+        $(this).fadeIn('fast'); // Animate the fade-in effect
+      });
     });
-  
-    // Click event for Convert link
-    document.getElementById("convert").addEventListener("click", function (e) {
-      e.preventDefault();
-      loadContentAndAnimate("convert.html");
-    });
-  
-    // Click event for Retain link
-    document.getElementById("retain").addEventListener("click", function (e) {
-      e.preventDefault();
-      loadContentAndAnimate("retain.html");
-    });
+  }
+
+  // Event handlers for the links
+  $('#prospect').click(function (e) {
+    e.preventDefault(); // Prevent default link behavior
+    loadAndAnimateContent('prospect.html');
   });
-  
+
+  $('#convert').click(function (e) {
+    e.preventDefault();
+    loadAndAnimateContent('convert.html');
+  });
+
+  $('#retain').click(function (e) {
+    e.preventDefault();
+    loadAndAnimateContent('retain.html');
+  });
+});
