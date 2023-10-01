2c59de7e14b3c5a48cb6b56e82cecf49b370575b
abdo754 <W0786364@myscc.ca>
2023-10-01 00:58:57 -0400

    3 seconds

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-// Method using $.getJSON()
-function loadTeamDataWithGetJSON() {
+// Method using $.ajax() with a delay
+function loadTeamDataWithAjaxAndDelay() {
     // Get the team div element
     var teamDiv = $("#team");
 
-    // Load JSON data using $.getJSON()
-    $.getJSON("team.json", function (data) {
-        // Clear any previous content in the team div
-        teamDiv.empty();
+    // Display "Loading..." message
+    teamDiv.html("Loading...");
 
-        // Loop through the data and append team members' information
-        $.each(data.members, function (index, member) {
-            var memberDiv = $("<div class='team-member'></div>");
-            var nameHeading = $("<h2>" + member.name + "</h2>");
-            var positionHeading = $("<h5>" + member.position + "</h5>");
-            var bioParagraph = $("<p>" + member.bio + "</p>");
+    // Use setTimeout to delay displaying the content for 3 seconds
+    setTimeout(function () {
+        // Load JSON data using $.ajax()
+        $.ajax({
+            url: "team.json",
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                // Clear any previous content in the team div
+                teamDiv.empty();
 
-            memberDiv.append(nameHeading, positionHeading, bioParagraph);
-            teamDiv.append(memberDiv);
+                // Loop through the data and append team members' information
+                $.each(data.members, function (index, member) {
+                    var memberDiv = $("<div class='team-member'></div>");
+                    var nameHeading = $("<h2>" + member.name + "</h2>");
+                    var positionHeading = $("<h5>" + member.position + "</h5>");
+                    var bioParagraph = $("<p>" + member.bio + "</p>");
+
+                    memberDiv.append(nameHeading, positionHeading, bioParagraph);
+                    teamDiv.append(memberDiv);
+                });
+            },
+            error: function () {
+                // Display an error message if the request fails
+                teamDiv.html("Error: Content could not be retrieved.");
+            }
         });
-    });
+    }, 3000); // 3000 milliseconds (3 seconds) delay
 }
 
 // Call the method in a ready function
 $(document).ready(function () {
-    loadTeamDataWithGetJSON();
+    loadTeamDataWithAjaxAndDelay();
 });
