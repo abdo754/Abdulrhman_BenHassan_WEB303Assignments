ebb7d7115cbfb8c10409b71a65fbe0899df320ff
abdo754 <W0786364@myscc.ca>
2023-10-01 01:07:13 -0400

    make is shorter

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Method using $.ajax() with a delay
 function loadTeamDataWithAjaxAndDelay() {
-    // Get the team div element
+   
     var teamDiv = $("#team");
 
-    // Display "Loading..." message
+    
     teamDiv.html("Loading...");
 
-    // Use setTimeout to delay displaying the content for 3 seconds
+    // content for 3 seconds
     setTimeout(function () {
         // Load JSON data using $.ajax()
         $.ajax({
@@ -14,10 +14,10 @@ function loadTeamDataWithAjaxAndDelay() {
             type: "GET",
             dataType: "json",
             success: function (data) {
-                // Clear any previous content in the team div
+               
                 teamDiv.empty();
 
-                // Loop through the data and append team members' information
+                //  data and append team members' information
                 $.each(data.members, function (index, member) {
                     var memberDiv = $("<div class='team-member'></div>");
                     var nameHeading = $("<h2>" + member.name + "</h2>");
@@ -29,14 +29,14 @@ function loadTeamDataWithAjaxAndDelay() {
                 });
             },
             error: function () {
-                // Display an error message if the request fails
+                //  if the request fails
                 teamDiv.html("Error: Content could not be retrieved.");
             }
         });
-    }, 3000); // 3000 milliseconds (3 seconds) delay
+    }, 3000); //(3 seconds) delay
 }
 
-// Call the method in a ready function
+// 
 $(document).ready(function () {
     loadTeamDataWithAjaxAndDelay();
 });
